3d8cd27c333dcecc0777dee0c4a60aca445a69bf
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2016-09-30 12:27:41 +0000

    before documentation

--- lib/scrapperFactory.py
+++ lib/scrapperFactory.py
@@ -14,8 +14,6 @@ from lib.scrappers import website2_sports
 class ScrapperFactory( object ):
 
     def factory( type, db, wpinfo, table, url, slug, log ):
-        #if type == "clarin_politica": return clarin_politica.ScrapperClarinPolitica( db, wpinfo, table, url, slug, log )
-        #if type == "clarin_boxeo": return clarin_boxeo.ScrapperClarinBoxeo( db, wpinfo, table, url, slug, log )
         if type == "website1_economy": return website1_economy.ScrapperWebsite1Economy( db, wpinfo, table, url, slug, log )
         if type == "website1_politics": return website1_politics.ScrapperWebsite1Politics( db, wpinfo, table, url, slug, log )
         if type == "website2_science": return website2_science.ScrapperWebsite2Science( db, wpinfo, table, url, slug, log )
